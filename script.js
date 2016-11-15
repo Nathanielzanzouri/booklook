17d2bc125671a92928fc567ec9323d4b1bd15a92
Nathaniel Zanzouri <NathanielZanzouri@Nathaniels-MacBook-Air.local>
2016-11-15 14:14:24 +0200

    booklook

---
+++ script.js
@@ -0,0 +1,33 @@
+
+
+ // var AddBooks = [] 
+ // {
+ // Title:$('.Title').val();
+ // Author:$('Author').val();
+ // Description:$('Description').val();
+ // Url:$('.Url').val();
+ // Number:$('Number').val();
+ // Minute:$('Minute').val();
+ // }
+
+
+var Books = []
+
+
+var addToArray = function () {
+	var book = {
+		Title: $('.Title').val(),
+		Author: $('Author').val(),
+		Description: $('Description').val(),
+		Url: $('.Url').val(),
+		Number: $('Number').val(),
+		Minute: $('Minute').val(),
+	}
+
+}
+
+$('button').on('click', function(){
+
+ alert("yo")
+
+ });
