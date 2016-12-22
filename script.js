1b161de7c094ac43e536e98c686b0abe9138a0cc
Nathaniel Zanzouri <NathanielZanzouri@Nathaniels-MacBook-Air.local>
2016-12-22 14:00:52 +0200

    Add main project files

--- script.js
+++ script.js
@@ -1,33 +1,77 @@
+var Weather = []
 
+var addToArray = function (data) {
+  
+  //create an object using information
+  var weather = {
+    description: data.weather[0].description, 
+    country: data.sys.country,
+    name: data.name,
+    temp: (data.main.temp)-273.15 + " Degree ",
+   
+  }
+  //push object to the array
+  Weather.push(weather)
 
- // var AddBooks = [] 
- // {
- // Title:$('.Title').val();
- // Author:$('Author').val();
- // Description:$('Description').val();
- // Url:$('.Url').val();
- // Number:$('Number').val();
- // Minute:$('Minute').val();
- // }
+  //empty input values
+  $('#city').val('');
 
+  //invoke addtoHTML
+addToHtml();
+}
 
-var Books = []
+var fetch = function(){
+  var city = $('#city').val();
+  var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=074767c3737ae2eec90174204f167021';
 
+  $.ajax({
+      method: "GET",
+      url: url,
+      dataType: "json",
+      success: function(data) {
 
-var addToArray = function () {
-	var book = {
-		Title: $('.Title').val(),
-		Author: $('Author').val(),
-		Description: $('Description').val(),
-		Url: $('.Url').val(),
-		Number: $('Number').val(),
-		Minute: $('Minute').val(),
-	}
+        addToArray(data);
+      },
+      error: function() {
+        console.log("error");
+      }
+  }); 
+}
+
+var addToHtml = function(){
+  $('.Forecast').empty()
+
+  var source = $('#weather-template').html();
+
+  var template = Handlebars.compile(source);
+
+  for(var i = 0; i < Weather.length; i ++){
+    var newHTML = template(Weather[i]);
+    $('.Forecast').append(newHTML); 
+  }
 
 }
 
-$('button').on('click', function(){
+// add comments
 
- alert("yo")
+$('#btncomment').on('click',function(){
+  alert('yoo')
+});
 
+
+
+$('#search').on('click', function(e){
+  e.preventDefault()
+
+ 
+  
+  //api request
+  fetch();
  });
+
+
+
+
+
+
+
